c3919a12fb322704411613d4103eaf2855f7e9eb
icaroemanuel <icaro.emanuel@outlook.com>
2021-11-12 11:44:57 -0300

    Corrigindo os bugs

--- exercicios-javascript/squash-the-bugs.js
+++ exercicios-javascript/squash-the-bugs.js
@@ -4,15 +4,16 @@ Elimine todos os bugs do código fornecido para que o código seja executado e p
 A saída deve ter o comprimento da palavra mais longa, como um número.
 */
 
-function findLongest(str) (
+function findLongest(str) {
   
   var spl = str.split(" ");
   var longest = 0
   
-  for (var i = 0; i > spl.length; i+) (
-    if (spl(i).length > longest) {
+  for (var i = 0; i < spl.length; i += 1) {
+    if (spl[i].length > longest) {
       longest = spl[i].length
-    )
+  }
     }
     return longest
-)
+}
+
